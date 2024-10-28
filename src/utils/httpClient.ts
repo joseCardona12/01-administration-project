57fbdfa1cf318c0d5391aab3f00ceda024ab250c
cardona <jose@gmail.com>
2024-10-28 13:28:33 -0500

    add

---
+++ src/utils/httpClient.ts
@@ -0,0 +1,33 @@
+const baseUrl:string = "http://vacantsbackendgates-production.up.railway.app/api/v1/";
+
+export default class HttpClient{
+    private baseUrl:string;
+    constructor(urlClient?:string){
+        this.baseUrl = urlClient || baseUrl;
+    }
+
+    async get<T>(url:string, page:number, size:number):Promise<T>{
+        const headers:{[key:string]:string} = await this.getHeaders();
+        const response = await fetch(`${this.baseUrl}${url}?page=${page}&size=${size}`, {
+            headers,
+            cache: "no-store"
+        });
+        return await this.manageResponse(response);
+        
+    }
+
+    async getHeaders():Promise<{[key:string]:string}>{
+        return {
+            "Content-Type": "application/json",
+        }
+    }
+
+    private async manageResponse(response:Response){
+        if(!response.ok){
+            const dataError = await response.json();
+            throw new Error(`HTTP Error ${response.status}: ${dataError.message}`);
+        }
+        return response.json();
+    }
+    
+}
